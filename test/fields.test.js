e3083783b5db06e643fc7b09e4d3728933c09e83
mparaiso <aikah@free.fr>
2013-12-22 19:23:42 +0100

    auto update Sun Dec 22 18:23:42 GMT 2013

---
+++ test/fields.test.js
@@ -0,0 +1,12 @@
+/*global describe,it*/
+"use strict";
+var fields=require('../lib/fields.js');
+describe("form.fields",function(){
+	describe(".Field",function(){
+		var field=new fields.Field();
+		it("instanciates",function(){
+
+		});
+	});
+})
+

431a1f1b8e0d2e148b637fa2162cd55739ec2977
mparaiso <aikah@free.fr>
2013-12-25 19:00:49 +0100

    auto update Wed Dec 25 18:00:49 GMT 2013

--- test/fields.test.js
+++ test/fields.test.js
@@ -1,6 +1,7 @@
 /*global describe,it*/
 "use strict";
-var expect=require('chai').expect;
+var chai=require('chai');
+var expect=chai.expect;
 var fields = require('../index').fields;
 describe('form.fields',function(){
     describe('.RadioGroup',function(){
@@ -8,7 +9,7 @@ describe('form.fields',function(){
             choices:['male','female','other'],
             attributes:{required:true}
         };
-        var radioGroup = new fields.RadioGroup("sex",options);
+        var radioGroup = new fields.RadioGroup("gender",options);
         radioGroup.setData('male');
         var html = radioGroup.toHTML();
         console.log(html);

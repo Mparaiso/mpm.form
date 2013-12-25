b106e20ae9edbe20334c06a932825c712dfa47fc
mparaiso <aikah@free.fr>
2013-12-25 20:03:15 +0100

    auto update Wed Dec 25 19:03:15 GMT 2013

--- test/fields.test.js
+++ test/fields.test.js
@@ -1,20 +1,22 @@
 /*global describe,it*/
 "use strict";
-var chai=require('chai');
-var expect=chai.expect;
+var chai = require('chai');
+var expect = chai.expect;
 var fields = require('../index').fields;
-var sys=require('sys');
-describe('form.fields',function(){
-    describe('.RadioGroup',function(){
-        var options ={
-            choices:['male','female','other'],
-            attributes:{required:true}
+var sys = require('sys');
+describe('form.fields', function () {
+    describe('.RadioGroup', function () {
+        var options = {
+            choices: ['male', 'female', 'other'],
+            attributes: {required: true}
         };
-        var radioGroup = new fields.RadioGroup("gender",options);
+        var radioGroup = new fields.RadioGroup("gender", options);
         radioGroup.setData('male');
         var html = radioGroup.toHTML();
-        expect(html).to.contain('male');
-        expect(html).to.contain('checked');
-        expect(radioGroup.getChoices()[0].attributes.checked).to.equal('checked');
+        it('male chould be checked', function () {
+            expect(html).to.contain('male');
+            expect(html).to.contain('checked');
+            expect(radioGroup.getChoices()[0].attributes.checked).to.equal('checked');
+        });
     });
 });
